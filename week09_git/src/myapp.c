e9ed5fc259aa692460abc0967045a537596f01c9
ansoon1010 <ibfwbo1010@gmail.com>
2025-11-16 21:45:09 +0900

    add calculate(), only print calculating...

--- week09_git/src/myapp.c
+++ week09_git/src/myapp.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+void calculate(int a, char op, int b);
+
 int main(void) {
+    int a, b;
+    char op;
+
     printf("~~~ Welcome to my calculator  ~~~\n");
+    printf("Enter expression (ex: 1 + 2): ");
+    scanf("%d %c %d", &a, &op, &b);
+
+    calculate(a, op, b);
+
     return 0;
+}
+
+void calculate(int a, char op, int b) {
+    printf("Calculating.....\n");
 }
